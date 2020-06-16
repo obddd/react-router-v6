826865269b13bfd16ac69912d718eb0183e2329b
Obaid <obaidulhaq20@gmail.com>
2020-06-17 01:31:42 +0500

    added component and init react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 const App = () => {
   return (
-    <div>
-      Bootcamp 2020 Project 3: Shopping Cart with Context API and React Hooks
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Directory from '../../components/directory/directory.component'
+
+const Homepage = () => {
+    return (
+        <div>
+            directory component
+        </div>
+    );
+};
+
+export default Homepage;
