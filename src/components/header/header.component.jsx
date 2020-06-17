72880e23a7fa7e7d8d6013a36a0e8bfc1ea665f0
Obaid <obaidulhaq20@gmail.com>
2020-06-17 20:17:49 +0500

    added header and shopping bag icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './header.style.css';
+
+import CartIcon from '../cart-icon/cart-icon.component'
 
 const Header = () => {
     return (
-        <div>
-          Header  
+        <div className='header'>
+            <div className='options'>
+                <Link className='option' to='/' >SHOP</Link>
+                <Link className='option' to='/' >ABOUT</Link>
+            <CartIcon />
+            </div> 
         </div>
     );
 };
+
+
 
 export default Header;
